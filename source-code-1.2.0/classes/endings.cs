e3a991bdbe29b9138f5718f06cada1c348b24603
Luke E <68721949+threesodas@users.noreply.github.com>
2021-03-25 08:27:50 -0500

    Add files via upload

---
+++ source-code-1.2.0/classes/endings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucy_Simulator_Nostalgic
+{
+    class endings
+    {
+        public static bool end_bestpetowner;
+        public static bool end_arcadesaresocheap;
+    }
+}
