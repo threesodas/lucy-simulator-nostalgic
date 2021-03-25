214da90498ec1e7fc232cf2240e7545777583fcd
Luke E <68721949+threesodas@users.noreply.github.com>
2021-03-25 11:17:43 -0500

    Add files via upload

---
+++ source-code-1.3.0/classes/gv.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucy_Simulator_Nostalgic
+{
+    class gv
+    {
+        public static string lucyType;
+        public static int cash;
+        public static int lucyRep;
+        public static string inventorySlotOne;
+        public static string inventorySlotTwo;
+        public static string inventorySlotThree;
+        public static bool loopActive;
+        public static string input;
+        public static string activity;
+        public static int cashLeftForArcade;
+        public static bool hasArcadePass;
+        public static int rng;
+        public static bool hasRaisePass;
+        public static string user;
+        public static int stored_cash;
+    }
+}
